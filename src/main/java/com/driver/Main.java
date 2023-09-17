19a0410cef7bf7cd43a6caffe4a7dbbf6eebe802
Arjun chand <chandarjun10@gmail.com>
2023-09-17 22:49:02 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
+class A{
+    String meth(){
+        return "Invoking method from class A";
+    }
+}
+class B extends A{
+    String meth(){
+        return "Method is overridden in Extendend class B";
+
+    }
+}
 public class Main {
-  
+    public static void main(String[] args) {
+        B Bobj = new B();
+        System.out.println(Bobj.meth());
+
+        B obj1 = new B();
+        System.out.println(obj1.meth());
+
+    }
 }
